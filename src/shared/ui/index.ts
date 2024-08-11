8c7836ea78e0f5fb44e4c59ce347b91f718842b1
Vatiba <anonimanonimus955@gmail.com>
2024-08-11 14:21:26 +0500

    :bug: fix: fixed import in shared ui

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,5 @@
-export { default as TextEditor } from './TextEditor';
 export { default as Container } from './container';
-export { default as Loading } from './loading';
+export { default as Loading } from './Loading';
 export { default as Pagination } from './pagination';
+export { default as TextEditor } from './TextEditor';
+
